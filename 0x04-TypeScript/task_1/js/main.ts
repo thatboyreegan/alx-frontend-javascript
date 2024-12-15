533f8abc415b015e2016b7d245f999e475f6c12b
thatboyreegan <reaganronald491@gmail.com>
2024-12-15 15:18:13 +0300

    add: interface Directors

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,9 +1,12 @@
 interface Teacher {
   readonly firstName: string,
-  readonly lastName: string;
+  readonly lastName: string,
   fullTimeEmployee: boolean,
   yearsOfExperience?: number,
   location: string,
   [key: string]: any,
 }
 
+interface Directors extends Teacher {
+    numberOfReports: number
+}
